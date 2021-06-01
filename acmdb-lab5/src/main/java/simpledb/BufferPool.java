751beeffa0e659a642d5192caa4d56f4649bdf6a
yujie6 <yujie6@sjtu.edu.cn>
2021-06-01 17:19:46 +0800

    finish ex5, failed on sys.BtreeTest

--- acmdb-lab5/src/main/java/simpledb/BufferPool.java
+++ acmdb-lab5/src/main/java/simpledb/BufferPool.java
@@ -141,7 +141,7 @@ public class BufferPool {
      * @param tid the ID of the transaction requesting the unlock
      * @param commit a flag indicating whether we should commit or abort
      */
-    public void transactionComplete(TransactionId tid, boolean commit)
+    public synchronized void transactionComplete(TransactionId tid, boolean commit)
         throws IOException {
         if (commit) {
             this.flushPages(tid);
